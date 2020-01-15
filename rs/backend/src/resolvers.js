f362ddeea5f547bd67d0a2dcede7cf9f45b7de9a
Thales <thales@carteiraglobal.com>
2020-01-15 14:50:25 -0300

    Resolver done

---
+++ rs/backend/src/resolvers.js
@@ -0,0 +1,18 @@
+const users = [
+    {id:1, name: 'Thales', email: 'thales@carteiraglobal.com'},
+    {id:2, name: 'Lucas', email: 'lucas@carteiraglobal.com'},
+    {id:3, name: 'Gabriel', email: 'gabriel@carteiraglobal.com'},
+    {id:4, name: 'Luan', email: 'luan@carteiraglobal.com'},
+    {id:5, name: 'Helder', email: 'helder@carteiraglobal.com'},
+    {id:6, name: 'Victor', email: 'victor@carteiraglobal.com'},
+]
+
+module.exports = {
+    Query: {
+        users: () => users,
+        user: (id) => users[1]
+    },
+    Mutation: {
+        createUser: () => users[0]
+    }
+}
